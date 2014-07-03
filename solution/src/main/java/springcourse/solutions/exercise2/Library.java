315ba739f13870d1b78ede82d94ddcbccf492460
Amit Tal <amitt03@yahoo.com>
2014-07-03 23:45:10 +0300

    Add exercise 2 solution

---
+++ solution/src/main/java/springcourse/solutions/exercise2/Library.java
@@ -0,0 +1,36 @@
+package springcourse.solutions.exercise2;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author Amit Tal
+ * @since 3/24/14
+ */
+public class Library {
+
+    private Logger logger = LoggerFactory.getLogger(Library.class);
+
+    // Need to get reference to BookDao
+    private BookDao bookDao;
+
+    public Library(BookDao bookDao) {
+        this.bookDao = bookDao;
+    }
+
+    @PostConstruct
+    public void open() {
+        if (bookDao.checkConnection()) {
+            logger.info("The library is now open [{}]", this.hashCode());
+        } else {
+            logger.info("The library is temporarily unavailable [{}]", this.hashCode());
+        }
+    }
+
+    public void close() {
+        logger.info("The library is now closed [{}]", this.hashCode());
+    }
+
+}
