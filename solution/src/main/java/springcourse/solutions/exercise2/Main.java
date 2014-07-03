055d88720434e77ce4290968395e437dafeba7b5
Amit Tal <amitt03@yahoo.com>
2014-07-04 00:26:49 +0300

    Update Main.java

--- solution/src/main/java/springcourse/solutions/exercise2/Main.java
+++ solution/src/main/java/springcourse/solutions/exercise2/Main.java
@@ -19,5 +19,15 @@ public class Main {
         Assert.assertEquals(libraryA, libA);
         Assert.assertEquals(libraryB, libB);
         Assert.assertNotEquals(libraryA, libraryB);
+        
+        // Prototype
+        Library prototypeLibraryA = context.getBean("prototypeLibrary", Library.class);
+        Library prototypeLibraryB = context.getBean("prototypeLibrary", Library.class);
+        Assert.assertNotEquals(prototypeLibraryA, prototypeLibraryB);
+        Assert.assertNotEquals(libraryA, prototypeLibraryA);
+        Assert.assertNotEquals(libraryB, prototypeLibraryA);
+        Assert.assertNotEquals(libraryA, prototypeLibraryB);
+        Assert.assertNotEquals(libraryB, prototypeLibraryB);
+        Assert.assertEquals(prototypeLibraryA.getBookDao(), prototypeLibraryB.getBookDao());
     }
 }
